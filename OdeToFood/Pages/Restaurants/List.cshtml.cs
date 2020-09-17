e391e4228c9f7cbc4fae04a1cf0b3c6938460397
Matt McMurray <MattMcMurray@users.noreply.github.com>
2020-09-17 09:46:46 -0500

    Start dotnet core tutorial

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+
+        public ListModel(IConfiguration config) {
+            this.config = config;
+        }
+
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = config["Message"];
+        }
+    }
+}
